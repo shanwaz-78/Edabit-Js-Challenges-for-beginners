9d6ab758feb962b971dfc4cb9ce0891e06ffc4d0
shanwaz-78 <111566952+shanwaz-78@users.noreply.github.com>
2023-07-05 18:12:40 +0530

    Update Half, Quarter and Eighth.js

--- Array's/Very Easy/Half, Quarter and Eighth.js
+++ Array's/Very Easy/Half, Quarter and Eighth.js
@@ -6,7 +6,6 @@
 // halfQuarterEighth(6) ➞ [3, 1.5, 0.75]
 
 // halfQuarterEighth(22) ➞ [11, 5.5, 2.75]
-
 // halfQuarterEighth(25) ➞ [12.5, 6.25, 3.125]
 // Notes
 // The order of the array is: half, quarter, eighth.
